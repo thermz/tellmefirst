51332d0e9c4a0bd5a60a1a3b71ede3948acb7972
Giuseppe Futia <giuseppe.futia@gmail.com>
2015-01-10 10:59:56 +0100

    ClassifyInterface.java: classify() is now called by the Client

--- back/rest/src/main/java/it/polito/tellmefirst/web/rest/interfaces/ClassifyInterface.java
+++ back/rest/src/main/java/it/polito/tellmefirst/web/rest/interfaces/ClassifyInterface.java
@@ -105,7 +105,9 @@ public class ClassifyInterface extends AbsResponseInterface {
     	return unchecked(()-> {
     			LOG.debug("[callClassify] - BEGIN");
     			Classifier classifier = (lang.equals("italian")) ? TMFListener.getItalianClassifier() : TMFListener.getEnglishClassifier();  
-    			List<String[]> topics = classifier.classify(textStr, numTopics, lang, wikihtml);
+			//We create an instance of the Client in order to manage different classification policies
+			Client client = new Client(classifier);
+			List<String[]> topics = client.classify(text, file, url, fileName, numTopics, lang);
     			LOG.debug("[callClassify] - END");
     			return topics;
 		});
